b0e59aafe646b4c0c72b7c771de5d1fb1893406d
Wyeha <snovavzloman@mail.ru>
2023-09-26 17:08:36 +0300

    2nd done

---
+++ Example2/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+int SumNaturalNumbers(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+
+    else
+    {
+        return M + SumNaturalNumbers(M + 1, N);
+    }
+}
+
+int M = 2;
+int N = 3;
+int sum = SumNaturalNumbers(M, N);
+Console.WriteLine("Сумма натуральных элементов от {0} до {1} равна {2}", M, N, sum);
