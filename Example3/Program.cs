be1632d3637e751f4cdb83b779c0823e0529d43e
Wyeha <snovavzloman@mail.ru>
2023-09-26 17:10:49 +0300

    3rd done

---
+++ Example3/Program.cs
@@ -0,0 +1,20 @@
+﻿int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+int m = 3;
+int n = 4;
+int result = Ackermann(m, n);
+Console.WriteLine("Ackermann({0}, {1}) = {2}", m, n, result);
